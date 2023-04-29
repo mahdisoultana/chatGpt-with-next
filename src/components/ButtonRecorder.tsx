ea3c684d41032499bf89e06f67975c71922bd749
Mahdi-Soultana <soultana.mahdi24@gmail.com>
2023-04-29 14:09:26 +0100

    adding overflow fixed for recording with layoutid animation

--- src/components/ButtonRecorder.tsx
+++ src/components/ButtonRecorder.tsx
@@ -1,4 +1,5 @@
 import { Message } from '@/hooks/types';
+import { motion } from 'framer-motion';
 import { useRef } from 'react';
 import { BsFillMicFill } from 'react-icons/bs';
 import { useReactMediaRecorder } from 'react-media-recorder';
@@ -7,7 +8,7 @@ function ButtonRecorder({ onSubmit }: { onSubmit: (msg: Message) => void }) {
   const urlRef = useRef('');
   const { status, startRecording, stopRecording } = useReactMediaRecorder({
     audio: true,
-    askPermissionOnMount: true,
+    // askPermissionOnMount: true,
     onStop(blobUrl) {
       if (
         typeof blobUrl == 'string' &&
@@ -20,32 +21,14 @@ function ButtonRecorder({ onSubmit }: { onSubmit: (msg: Message) => void }) {
           sender: 'me',
         });
         urlRef.current = blobUrl;
-        const audio = new Audio(blobUrl);
-        console.log(audio);
       }
     },
   });
-  // return (
-  //   <ReactMediaRecorder
-  //     audio
-  //     render={({ status, startRecording, stopRecording, mediaBlobUrl }) => {
-  //       if (
-  //         typeof mediaBlobUrl == 'string' &&
-  //         mediaBlobUrl.length > 0 &&
-  //         mediaBlobUrl !== urlRef.current
-  //       ) {
-  //         onSubmit({
-  //           message: mediaBlobUrl,
-  //           type: 'audio',
-  //           sender: 'me',
-  //         });
-  //         urlRef.current = mediaBlobUrl;
-  //         const audio = new Audio(mediaBlobUrl);
-  //         console.log(audio);
-  //       }
+
   return (
     <>
       <button
+        type="button"
         className={` w-full h-full  rounded-full flex-shrink-0 flex items-center justify-center   ${
           status == 'recording' && 'animate-pulse'
         }`}
@@ -57,13 +40,36 @@ function ButtonRecorder({ onSubmit }: { onSubmit: (msg: Message) => void }) {
           }
         }}
       >
-        <BsFillMicFill className=" text-3xl text-blue-500 " />
+        {status == 'recording' ? (
+          <div className="fixed top-0 left-0 bg-black/60 w-full h-full flex items-center justify-center">
+            {[-1, 3].map((i) => (
+              <motion.span
+                key={i}
+                animate={{
+                  opacity: [1, 0.1],
+                  scale: [0.5, 2],
+                  transition: {
+                    duration: 3,
+                    delay: i * 0.5,
+                    repeat: Infinity,
+                    repeatType: 'loop',
+                  },
+                }}
+                className="absolute w-[100px] h-[100px] bg-gray-500/20  rounded-full  "
+              ></motion.span>
+            ))}
+            <motion.div layoutId="recIcon">
+              <BsFillMicFill className=" text-[6rem] text-green-600 animate-pulse " />
+            </motion.div>
+          </div>
+        ) : (
+          <motion.div layoutId="recIcon">
+            <BsFillMicFill className=" text-3xl text-blue-500 " />
+          </motion.div>
+        )}
       </button>
     </>
   );
-  //     }}
-  //   />
-  // );
 }
 
 export default ButtonRecorder;
