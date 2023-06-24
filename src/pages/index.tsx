54006a86154da9325c9afe0cdfaaed2377695ab1
mahdisoultana <mahdi@omicmd.com>
2023-06-25 00:30:15 +0100

    connecting chatgpt to our app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,48 @@
 import Form from '@/components/form';
 import Message from '@/components/message';
+import MessageLoading from '@/components/message/Loading';
 import { useMessages } from '@/hooks/store';
 import { Message as MessageType } from '@/hooks/types';
 import axios from 'axios';
+import { AnimatePresence } from 'framer-motion';
 import { GetServerSideProps } from 'next';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 async function requestMessage(request: MessageType) {
-  if (request.sender == 'me') {
-    //we need to ask chagpt with this prompt message
-    console.log(
-      'we need to ask chagpt with this prompt message🐱‍🏍🐱‍🏍',
-      `''` + request.message + `''`,
-    );
-    // return fetch(...).then(res=>res)
-  }
+  const res = await axios.post('/api/chatgpt', {
+    message: request.message,
+  });
+
+  console.log(res.data);
+  return res.data;
 }
+type StatusType = 'idle' | 'loading' | 'error' | 'success';
 function Home(props: GetServerSideProps) {
-  useEffect(() => {
-    axios.get('/api/chatgpt').then((res) => {
-      console.log(res.data);
-    });
-  }, []);
-  // console.log(props);
   const { messages, setMessages } = useMessages();
-  const onSubmit = useCallback((msg: MessageType) => {
+  const [status, setStatus] = useState<StatusType>('idle');
+
+  const onSubmit = useCallback(async (msg: MessageType) => {
     setMessages(msg);
-    requestMessage(msg);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    try {
+      setStatus('loading');
+      const data = await requestMessage(msg);
+      setMessages({ type: 'text', message: data.response, sender: 'chatGPT' });
+      setStatus('success');
+    } catch (error) {
+      setStatus('error');
+    }
   }, []);
 
   return (
     <div className="min-h-screen w-full bg-gray-900 flex items-center justify-center text-white">
-      <article className="relative max-w-xl w-full min-h-screen rounded  m-auto py-6 px-2 space-y-4 pb-24">
+      <article className="relative max-w-xl w-full min-h-screen rounded  m-auto py-6 px-2 space-y-2 pb-24">
         {messages.map((item, i) => (
           <Message i={i} item={item} key={i} />
         ))}
+
+        <AnimatePresence>
+          {status == 'loading' && <MessageLoading />}
+        </AnimatePresence>
         <Form onSubmit={onSubmit} />
       </article>
     </div>
